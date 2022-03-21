e8525de229aebf47a932d240058def566d35ff99
YeonKyung Lee <yeonk33@gmail.com>
2022-03-22 03:10:32 +0900

    블로그를 CBV로 리팩토링

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,35 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post' : post,
+#         }
+#     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
+class PostList(ListView):
+    model = Post
+
+
+class PostDetail(DetailView):
+    model = Post
