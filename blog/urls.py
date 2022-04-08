31e7ae9aac6b212c4b5798e76271e078f9592012
YeonKyung Lee <yeonk33@gmail.com>
2022-04-08 17:42:11 +0900

    post에 tag 추가, 태그 뷰 생성

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     #path('<int:pk>/', views.single_post_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     #path('', views.index),
-    path('category/<str:slug>/', views.show_category_posts)
+    path('category/<str:slug>/', views.show_category_posts),
+    path('tag/<str:slug>/', views.show_tag_posts)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 
@@ -63,5 +63,18 @@ def show_category_posts(request, slug):
         'no_category_post_count' : Post.objects.filter(category=None).count(),
         'category' : category,
         'post_list' : post_list
+    }
+    return render(request, 'blog/post_list.html', context)
+
+
+def show_tag_posts(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    context = {
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count(),
+        'tag': tag,
+        'post_list': post_list
     }
     return render(request, 'blog/post_list.html', context)
